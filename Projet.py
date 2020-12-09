cb5337fd8935a837903a722736a9643005ca436b
AngelaHdr <angelahermosillad@gmail.com>
2020-12-09 13:54:18 +0100

    no message

--- Projet.py
+++ Projet.py
@@ -526,31 +526,3 @@ def prochain_joueur(joueur):
     nom=list(joueur.keys())
     return nom
 
-
-#Programme principal
-    #plateau de jeu avec les bonus
-plateau=init_bonus()
-
-    #dictoner avec les valeurs
-dico=init_dico()
-
-    #sac avec les jetons
-pioche=init_pioche(dico)
-
-    #liste de mots existants
-motsfr=generer_dico("FICHIERLETTRE.txt")
-
-    #dictioner avec les mots placés au tableau et ses valeurs
-mots_tableau={}
-
-    #dictioner avec les prenoms des joueurs, ses mains et ses points
-joueur=joueurs(pioche)
-print(joueur)
-
-    #liste avec les prenoms des joueurs en ordre
-nom=prochain_joueur(joueur)
-print(nom)
-
-    #affichage du plateau vide
-for ligne in plateau:
-    print(*ligne, sep="|")
